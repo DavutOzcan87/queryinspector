4ba5edf2b69df704680c55ab63d191671fd2d093
Your Name <davutozcan87@gmail.com>
2021-06-06 21:56:59 +0300

    deployment completed

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -3,7 +3,4 @@ rm -rf dist
 npm run-script build
 docker build . --no-cache -t queryinspector
 
-docker tag queryinspector registry.digitalocean.com/dwt-development/queryinspector
-docker push registry.digitalocean.com/dwt-development/queryinspector
-
 docker run -it -p 88:80 queryinspector
